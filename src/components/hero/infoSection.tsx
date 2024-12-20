9f4b26213a77f06ba6c376a7c169a038e25ecb45
Josafat Jimenez <josa221001@gmail.com>
2024-12-20 16:17:55 -0600

    feat: branding carousel, team members, and hero

---
+++ src/components/hero/infoSection.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export const InfoSection = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.75, ease: "easeOut" }}
+      className="text-center lg:text-left"
+    >
+      <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-800 leading-tight">
+        Bienvenido a <span className="text-blue-600">Guayaba Devs</span>
+      </h1>
+      <p className="mt-4 text-lg sm:text-xl text-gray-600 max-w-lg mx-auto lg:mx-0">
+        Conectamos talento y oportunidades desde cualquier parte del mundo.
+      </p>
+      <div className="mt-6">
+        <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-lg hover:bg-blue-500 transition-transform transform hover:scale-105">
+          Únete Ahora
+        </button>
+      </div>
+    </motion.div>
+  );
+};
---
+++ src/components/marquee/logoSection.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Marquee from "./marquee";
+import "./marquee.css";
+
+interface Logo {
+  src: string;
+  alt: string;
+}
+
+export const LogoSection = () => {
+  const logos: Logo[] = [
+    { src: "/NAO_Tecmilenio.png", alt: "Digital Nao Logo" },
+    { src: "/github-campus-experts.svg", alt: "Github Campus Experts Logo" },
+    { src: "/backpack.png", alt: "Student Developer Pack" },
+    { src: "/notion-logo.png", alt: "Notion" },
+  ];
+
+  return (
+    <div className="w-full bg-gray-900 text-white py-2 bg-gradient-to-r from-primary to-secondary">
+      <div className="text-center mb-8 px-6 md:px-4">
+        <h2 className="text-2xl font-bold md:text-4xl mb-4 md:mt-14">
+          Con el respaldo de líderes en la industria
+        </h2>
+        <p className="text-lg mt-2 md:text-xl">
+          Juntos fortalecemos la innovación y el crecimiento de la comunidad
+          tecnológica
+        </p>
+      </div>
+      <div className="overflow-hidden w-full py-5 h-20 marquee md:mb-4">
+        <Marquee speed={50} gradient={"false"}>
+          {logos.map((logo, index) => (
+            <div key={index} className="inline-block mx-5 text-center">
+              <img
+                src={logo.src}
+                alt={logo.alt}
+                className="h-12 md:h-16 object-contain grayscale-0 aspect-auto"
+              />
+            </div>
+          ))}
+        </Marquee>
+      </div>
+    </div>
+  );
+};
