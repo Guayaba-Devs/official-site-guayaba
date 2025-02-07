64d6dffd6625d2741a773398426569eb2d00913d
Josafat Jimenez <josa221001@gmail.com>
2025-02-07 15:57:37 -0600

    fix: path context

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+    ? `${process.env.NEXT_PUBLIC_BASE_PATH}/`
+    : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
